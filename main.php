10e17d09be24362043cbc42276439497b7a9fd47
Lluis Roca <114991293+lluisroca@users.noreply.github.com>
2022-10-07 10:20:39 +0000

    Hello world java + python

--- main.php
+++ main.php
@@ -1,1 +1,29 @@
+<?php
+echo "BUENAS TARDES \n";
+$prova1 = 2;
+$$prova1 = 4;
+$prova2 = 1;
+$prova3 = FALSE;
+
+echo "VALOR DE PRUEBA 1:".$prova1."\n";
+echo "VALOR DE PRUEBA 1:".$$prova1;
+echo $prova3."\n";
+  
+if ($prova1 + $prova2 == 4){
+  echo "BIEN";
+}
+  
+else{
+  echo "MAL";
+  $prova3 = TRUE;
+    
+  if ($prova3 == TRUE){
+    echo " jeje ";
+  }
+}
+
+echo $prova1;
+#echo $prova3;
+
+?>
 
